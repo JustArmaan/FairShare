972328799913edb43f82f79e12ef5a44fd90e513
Vakiyama <vakiyama@outlook.com>
2024-03-30 23:05:04 -0700

    feat: basic user seeding

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,1 +1,23 @@
+import { faker } from '@faker-js/faker';
+import { db } from './client';
+import { users } from './schema/users';
 
+// seeding for users
+
+await db.delete(users);
+
+await db
+  .insert(users)
+  .values([
+    { name: 'bob' },
+    { name: 'gilgamesh devourer of worlds' },
+    { name: 'steve' },
+    { name: 'gragnoroth, cousin of gilgamesh' },
+    { name: 'joe 2' },
+    { name: faker.person.firstName() },
+    { name: faker.person.firstName() },
+    { name: faker.person.firstName() },
+    { name: faker.person.firstName() },
+    { name: faker.person.firstName() },
+    { name: faker.person.firstName() },
+  ]);
