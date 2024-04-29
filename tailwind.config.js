1d970cdf9e544dcf4fdaaa5e280c475fb94348fe
Vakiyama <vakiyama@outlook.com>
2024-04-29 15:32:22 -0700

    feat: tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {},
+    colors: {
+      'primary-dark-grey': '686868',
+      'primary-grey': 'E7E7E7',
+      'accent-blue': '4CAEF8',
+      'accent-purple': '9079FF',
+      'accent-red': 'FF7969',
+      'accent-yellow': 'FFCF72',
+      'accent-green': '00D59C',
+      'pure-white': 'FFFFFF',
+      'font-grey': '8D8D93',
+      'card-black': '1C1C1E',
+    },
   },
   plugins: [],
 };
