a2928f0940e3b6fe7d8268987fe7bc87881c118e
Vakiyama <vakiyama@outlook.com>
2024-04-29 10:06:12 -0700

    fix: remove broken home.ts

--- src/server/views/pages/Home.tsx
+++ src/server/views/pages/Home.tsx
@@ -3,9 +3,10 @@ import { Html } from '../components/Html';
 export const Home = () => {
   return (
     <Html>
-      <h1>Hello IDSP!acc</h1>
+      <h1>Hello IDSP!</h1>
       <div id="logged_out_view">
         <a href="/login">Sign in</a>
+        <br />
         <a href="/register">Register</a>
       </div>
     </Html>
