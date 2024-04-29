e758c7f8937031deb9b8580ea112a9e5deaa1815
JustArmaan <singharmaanbrar@gmail.com>
2024-04-29 15:32:14 -0700

    tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ export default {
   theme: {
     extend: {},
     colors: {
-      
+      "PositiveNumber": "#37BA3C",
+      "NegativeNumber": "#DD4242",
+      "FontBlack": "#2F2F2F",
+      "FontOffWhite": "#F9F9F9",
+      "PrimaryBlackPage": "#181616",
+      "PrimaryBlack": "#343434",
+      "PrimaryFadedBlack": "#4E4E4E",
+      "PrimaryFadedBlack": "#4E4E4E",
     }
   },
   plugins: [],
