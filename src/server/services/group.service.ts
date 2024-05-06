d192a8af98de6c0e5b8060a413ccf3cf2a33c0e5
byron dray <byrondray8@gmail.com>
2024-05-06 14:45:00 -0700

    fix the group type

--- src/server/services/group.service.ts
+++ src/server/services/group.service.ts
@@ -7,11 +7,11 @@ let db = getDB();
 export async function getGroup(groupId: string) {
   try {
     const group = await db.select().from(groups).where(eq(groups.id, groupId));
-    if (group) return group[0];
-    return null;
+    return group[0];
   } catch (error) {
     console.error(error);
+    return null;
   }
 }
 
-export type GroupSchema = Awaited<ReturnType<typeof getGroup>>;
+export type GroupSchema = NonNullable<Awaited<ReturnType<typeof getGroup>>>;
