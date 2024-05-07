3cb4c9975568f995f3079762dcd9e8396ee915ba
Vakiyama <vakiyama@outlook.com>
2024-05-07 13:25:25 -0700

    work on groups list page

--- src/server/views/pages/Group/GroupPage.tsx
+++ src/server/views/pages/Group/GroupPage.tsx
@@ -1,5 +1,10 @@
+import type { GroupSchema } from '../../../services/group.service';
 import { GroupItem } from './components/GroupItem';
 
+const sampleGroup: GroupSchema = {
+  id: '',
+  name: 'Family',
+};
 export const GroupPage = () => {
   return (
     <div class="p-6 animate-fade-in">
@@ -8,10 +13,31 @@ export const GroupPage = () => {
         <img class="h-full" src="icons/edit.svg" alt="edit icon" />
       </div>
       <div class="px-4 py-2 mt-4 rounded-lg bg-primary-black">
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
-        <GroupItem />
+        <GroupItem
+          group={{
+            id: '',
+            name: 'Family',
+          }}
+          tailwindColorClass="accent-red"
+        />
+        <div class="mt-3 h-px rounded-full bg-primary-dark-grey" />
+        <GroupItem
+          group={{
+            id: '',
+            name: 'Friends',
+          }}
+          tailwindColorClass="accent-blue"
+        />
+        <div class="mt-3 h-px rounded-full bg-primary-dark-grey" />
+        <GroupItem
+          group={{
+            id: '',
+            name: 'Friday Night',
+          }}
+          tailwindColorClass="accent-green"
+          temp
+        />
+        <div class="mt-3 h-px rounded-full bg-primary-dark-grey" />
       </div>
     </div>
   );
