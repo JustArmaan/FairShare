a547bb11c62f3559336e43c69a495e6f9162b81d
Vakiyama <vakiyama@outlook.com>
2024-05-13 11:01:23 -0700

    remove express session

--- src/server/middleware/express.middleware.ts
+++ src/server/middleware/express.middleware.ts
@@ -1,5 +1,4 @@
 import express, { type Express } from 'express';
-import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import type { UserSchema } from '../interface/types';
 import { getUser } from '../routes/authRouter';
@@ -15,18 +14,6 @@ export const configureApp = (app: Express) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.static('~/public'));
-  app.use(
-    session({
-      secret: "secret",
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: true,
-        maxAge: 24 * 60 * 60 * 1000,
-      },
-    })
-  );
   app.use(cookieParser());
 
   app.use('/', getUser, (_, __, next) => {
