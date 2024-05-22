6da3fd7c9f8d677f724883c1488bb2c99611185b
JustArmaan <singharmaanbrar@gmail.com>
2024-05-22 11:24:06 -0700

    create notification componenet

--- src/server/views/pages/Notifications/components/Reminder.tsx
+++ src/server/views/pages/Notifications/components/Reminder.tsx
@@ -1,13 +1,34 @@
 export const Reminder = () => {
   return (
-    <div class="p-6 animate-fade-in">
-      <button class="transaction rounded-xl w-full h-fit">
-      <div class="text-font-off-white font-semibold mb-2">
-        2min ago
+    <div class="animate-fade-in">
+  <button id={``} class={`transaction rounded-xl w-full h-fit`}>
+    <div class={`rounded-2xl mt-2`}>
+      <div class="hover:-translate-y-0.5 cursor-pointer transition-all mt-4 bg-primary-black p-2 rounded-xl shadow-md mb-1 flex items-center justify-between relative">
+        <div class="flex items-center">
+          <div class={`p-3 pl-4 pr-4 mr-4 bg-category-color-0 rounded-xl`}>
+            <div class="flex items-center justify-center w-10 h-10">
+              {/* categories same as tranactions */}
+              <img src="/icons/dentist.svg" alt="" class="w-10" />
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <p class="text-font-off-white font-semibold w-fit flex items-center">
+              Account:{" "}
+              {/* account number */}
+              <span class="text-font-off-white font-normal ml-1">#1132457</span>
+              <span class="absolute text-xs text-font-grey m-2.5 items-end right-0 top-0">2m ago</span>
+            </p>
+            <p class="text-font-off-white">Will contain some message</p>
+            {/* Some function that checks status and either choses blinking blue or check mark */}
+            <span class="animate-ping absolute inline-flex h-2 w-2 rounded-full bg-accent-blue opacity-75 items-end right-5 top-[50%]"></span>
+            <span class="absolute inline-flex h-2 w-2 rounded-full bg-accent-blue opacity-75 items-end right-5 top-[50%]"></span>
+          </div>
         </div>
-        
-      </button>
+      </div>
     </div>
+  </button>
+</div>
+
   );
 };
 
