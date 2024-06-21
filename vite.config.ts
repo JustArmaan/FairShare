50c324bfcebf03a69973d2d8b27b853eddf5ff15
Vakiyama <vakiyama@outlook.com>
2024-06-21 10:20:06 -0700

    more config

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default {
         theme_color: "#181616",
         icons: [
           {
-            src: "public/pwa-192x192.png", // need to create these ! app won't work without them.
+            src: "pwa-192x192.png", // need to create these ! app won't work without them.
             sizes: "192x192",
             type: "image/svg+xml",
           },
           {
-            src: "public/pwa-192x192.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/svg+xml",
           },
