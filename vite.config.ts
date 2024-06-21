399bacca0424a1e0dce04e24e0f8df1d516b6694
Vakiyama <vakiyama@outlook.com>
2024-06-21 09:51:51 -0700

    work on pwa configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,31 @@
+import { VitePWA } from "vite-plugin-pwa";
+
 export default {
   build: {
     sourcemap: true,
   },
+  plugins: [
+    VitePWA({
+      includeAssets: ["public/favicon.svg"],
+      manifest: {
+        name: "FairShare",
+        short_name: "FairShare",
+        description:
+          "A personal finance application with social, bill splitting features.",
+        theme_color: "#181616",
+        icons: [
+          {
+            src: "public/pwa-192x192.png", // need to create these ! app won't work without them.
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "public/pwa-192x192.png",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
 };
