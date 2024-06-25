48f0f740002229d52215d987b90ffe6ef610e146
Vakiyama <vakiyama@outlook.com>
2024-06-24 17:11:24 -0700

    middleware error handling work

--- src/server/middleware/errorHandler.middleware.ts
+++ src/server/middleware/errorHandler.middleware.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import fs from "fs/promises";
+import path from "path";
+import { formatDate } from "../views/pages/transactions/components/Transaction";
 
 interface ErrorWithStatus extends Error {
   status?: number;
@@ -13,6 +16,16 @@ export function errorHandler(
   console.error(error);
   const status = error.status || 500;
   res.statusCode = status;
+  /*
+  fs.writeFile(
+    path.join(__dirname, "./error.log"),
+    JSON.stringify({
+      stack: error.stack,
+      time: formatDate(new Date().toISOString()),
+    }),
+    "utf-8"
+  ).then(() => console.log("Wrote to error.log"));
+  */ //imrpove this
   console.error(`status: ${status}`);
   res.redirect(`/error/${status}`);
   // next();
