6d781bde089e87df48ab04047208f82eda5cd34a
Vakiyama <vakiyama@outlook.com>
2024-07-05 09:49:48 -0700

    clean up unused files with svg feat

--- scripts/svgToComponent.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs/promises";
-import path from "path";
-import os from "os";
-
-const { EOL } = os;
-
-async function convertToComponent(svgPath: string) {
-  const svgContents = await fs.readFile(svgPath, "utf8");
-}
