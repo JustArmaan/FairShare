995f5ba0004a4007a386496b4f16f7a88d746af4
JustArmaan <singharmaanbrar@gmail.com>
2024-07-19 12:18:01 -0400

    add new category colors/change blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,28 +34,28 @@ export default {
       },
     },
     colors: {
-      "category-color-0": "#ff4d4d",
-      "category-color-1": "#ff8b4d",
-      "category-color-2": "#ffcb4d",
-      "category-color-3": "#bcc902",
-      "category-color-4": "#b6ff4d",
-      "category-color-5": "#36b50e",
-      "category-color-6": "#4dff61",
-      "category-color-7": "#4dffa1",
-      "category-color-8": "#4dffdf",
-      "category-color-9": "#4ddfff",
-      "category-color-10": "#4da1ff",
-      "category-color-11": "#4d61ff",
-      "category-color-12": "#774dff",
-      "category-color-13": "#b64dff",
-      "category-color-14": "#f54dff",
-      "category-color-15": "#ff4dca",
-      "category-color-16": "#ff4d8b",
+      "category-color-0": "#ff4d4d",//red/guava pink
+      "category-color-1": "#ff8b4d",//orange
+      "category-color-2": "#ffcb4d",//yellow/
+      "category-color-3": "#bcc902",//lime
+      "category-color-4": "#b6ff4d",//lime green
+      "category-color-5": "#36b50e",//green
+      "category-color-6": "#4dff61",//light green
+      "category-color-7": "#4dffa1",//teal
+      "category-color-8": "#4dffdf",//cyan
+      "category-color-9": "#26ccd7",//light blue(4ddfff)
+      "category-color-10": "#0052ba",//blue-saphire blue(4da1ff)
+      "category-color-11": "#4d61ff",//indigo
+      "category-color-12": "#774dff",//violet
+      "category-color-13": "#b64dff",//purple
+      "category-color-14": "#f54dff",//pink
+      "category-color-15": "#ff4dca",//hot-pink
+      "category-color-16": "#ff4d8b",//coral
       "primary-grey": "#E7E7E7",
       "accent-blue": "#4CAEF8",
       "accent-purple": "#9079FF",
       "accent-red": "#FF7969",
-      "accent-yellow": "#FFCF72",
+      "accent-yellow": "#F4C56A",
       "accent-green": "#00D59C",
       "accent-teal": "#42DCD3",
       "pure-white": "#FFFFFF",
@@ -71,12 +71,25 @@ export default {
       "primary-faded-black": "#4E4E4E",
       "primary-dark-grey": "#686868",
       "card-red": "#e60000",
+      "periwinkle": "#adbaf2",
+      "grass-green": "#71c060",
+      "guava-pink": "#fc5878",
+      "citron": "#eedc7c",
+      "robin-egg-blue": "#26ccd7",
+      "plum": "#8e4696",
+      "dark-cyan": "#008b8b",
+      "poppy-red": "#d83434",
+      "tangerine": "#f09c4b",
+      "sappire-blue": "#0052ba",
+      "pink-blush": "#efaff0",
+      "forest-green": "#066839",
+      "redwood": "#be4d4b",
     },
     fontFamily: {
       sans: ["Work Sans", "sans-serif"],
     },
   },
-  safelist: [
+  safelist: [ // Need to add new category colors here (periwinkle, )
     "bg-category-color-0",
     "bg-category-color-1",
     "bg-category-color-2",
