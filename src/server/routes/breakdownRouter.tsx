fb4eedcab631045448abdb9171a97e566d08962b
byron dray <byrondray8@gmail.com>
2024-07-24 12:31:55 -0700

    fix cash transactions on the breakdown page

--- src/server/routes/breakdownRouter.tsx
+++ src/server/routes/breakdownRouter.tsx
@@ -6,54 +6,67 @@ import {
   getAccountWithCurrentMonthTransactions,
   getAccountWithMonthTransactions,
   getAccountWithTransactions,
+  getCashAccountWithTransaction,
 } from "../services/plaid.service";
+import { getOrCreateCashAccountForUser } from "../utils/getOrCreateCashAccount";
 
 const router = express.Router();
 
 router.get("/page/:accountId", getUser, async (req, res) => {
   const { month, year } = req.query;
   let result;
+  let allTransactions;
+  let uniqueYearMonth = new Set();
   if (month && year) {
     result = await getAccountWithMonthTransactions(
       req.params.accountId,
       Number(year),
       Number(month)
     );
+    allTransactions = await getAccountWithTransactions(req.params.accountId);
+    allTransactions?.transactions?.map((transaction) => {
+      const date = new Date(transaction.timestamp as string);
+      const yearMonth = `${date.getFullYear()}-${(
+        "0" +
+        (date.getMonth() + 1)
+      ).slice(-2)}`;
+      uniqueYearMonth.add(yearMonth);
+    });
   } else {
     result = await getAccountWithCurrentMonthTransactions(req.params.accountId);
+    allTransactions = await getAccountWithTransactions(req.params.accountId);
+    allTransactions?.transactions?.map((transaction) => {
+      const date = new Date(transaction.timestamp as string);
+      const yearMonth = `${date.getFullYear()}-${(
+        "0" +
+        (date.getMonth() + 1)
+      ).slice(-2)}`;
+      uniqueYearMonth.add(yearMonth);
+    });
   }
 
-  const allTransactions = await getAccountWithTransactions(
-    req.params.accountId
-  );
+  if (!result) {
+    const cashAccount = await getOrCreateCashAccountForUser(req.user!.id);
 
-  // if (allTransactions) {
-  //   const sortedMonthlyTransactions = allTransactions.transactions.reduce(
-  //     (acc, transaction) => {
-  //       const keyArr = transaction.timestamp?.split("-");
-  //       if (!keyArr) return acc;
-  //       const key = `${keyArr[0]}-${keyArr[1]}`;
-  //       if (!acc[key]) acc[key] = [];
-  //       acc[key].push(transaction);
-  //       return acc;
-  //     }
-  //   );
-  // }
-
-  const uniqueYearMonth = new Set();
-
-  allTransactions?.transactions?.map((transaction) => {
-    const date = new Date(transaction.timestamp as string);
-    const yearMonth = `${date.getFullYear()}-${(
-      "0" +
-      (date.getMonth() + 1)
-    ).slice(-2)}`;
-    uniqueYearMonth.add(yearMonth);
-  });
+    if (!cashAccount)
+      throw new Error("There is a problem retrieving your cash account");
+    result = await getCashAccountWithTransaction(cashAccount.account_id);
+    allTransactions = result?.transactions;
+    allTransactions?.map((transaction) => {
+      const date = new Date(transaction.timestamp as string);
+      const yearMonth = `${date.getFullYear()}-${(
+        "0" +
+        (date.getMonth() + 1)
+      ).slice(-2)}`;
+      uniqueYearMonth.add(yearMonth);
+    });
+  }
 
   if (!result) throw new Error("404");
+
   const html = renderToHtml(
     <BreakdownPage
+      // @ts-ignore
       transactions={result.transactions}
       accountName={result.name}
       accountId={req.params.accountId}
