c68078b8c764f5384ffc33be798d27eed3727b19
Vakiyama <vakiyama@outlook.com>
2024-08-30 16:08:25 -0700

    add git token

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 bun i
 bun tailwind-build 
+export NIX_CONFIG="access-tokens = github.com=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"
 bun run build .
 pip install -r src/py-receipt-server/requirements.txt --break-system-packages
 python -m src.py-receipt-server.app &
